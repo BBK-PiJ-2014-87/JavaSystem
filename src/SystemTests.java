4a14ddb0eb96c603a98f58977055b2c80674377f
Vladimirs Ivanovs <ivanovs223@gmail.com>
2015-03-16 20:59:46 +0000

    Added new variables

--- src/SystemTests.java
+++ src/SystemTests.java
@@ -1,8 +1,22 @@
+/**
+ * Various interesting methods in Java.
+ * @author Workstation
+ *
+ */
 public class SystemTests {
 	public static void main(String[] args) {
 
 		long heapSize = Runtime.getRuntime().totalMemory();
 		System.out
-				.println("Current heap size in MB: " + heapSize / 1024 / 1024);
+				.println("Current heap size in MB: " + heapSize / 1000 / 1000);
+		
+		
+		long heapMaxSize = Runtime.getRuntime().maxMemory();
+		System.out
+		.println("Maximum heap size in MB: " + heapMaxSize / 1000 / 1000);
+
+		long heapFreeSize = Runtime.getRuntime().freeMemory(); 
+		System.out
+		.println("Maximum heap size in MB: " + heapFreeSize / 1000 / 1000);
 	}
 }
